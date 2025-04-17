db0f881ba77c276652c92dfd5d5b9e03d3520e0b
RaidTheWeb <raidtheweb@disr.it>
2025-04-17 22:54:46 +1200

    remove runtime (libraries are static now)

--- rt.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Very skeleton program designed only to facilitate easy usage of the runtime libraries.
-
-echo "Running with Omicron Engine's runtime libraries..."
-
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(realpath rt/)" $1 ${@:2}
